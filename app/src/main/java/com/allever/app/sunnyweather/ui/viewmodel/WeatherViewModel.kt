11a97fa208ffb30ceac8488ffb4d972134720c5b
devallever <devallever@163.com>
2020-06-22 00:30:27 +0800

    FEATURE：获取天气ViewModel层

---
+++ app/src/main/java/com/allever/app/sunnyweather/ui/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,26 @@
+package com.allever.app.sunnyweather.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+import com.allever.app.sunnyweather.function.Repository
+import com.allever.app.sunnyweather.function.model.Location
+
+class WeatherViewModel: ViewModel() {
+
+    private val locationLiveData = MutableLiveData<Location>()
+
+    var lng = ""
+
+    var lat = ""
+
+    var placeName = ""
+
+    val weatherLiveData = Transformations.switchMap(locationLiveData) {
+        Repository.fetchWeather(it.lng, it.lat)
+    }
+
+    fun refreshWeather(lng: String, lat: String) {
+        locationLiveData.value = Location(lng, lat)
+    }
+}
